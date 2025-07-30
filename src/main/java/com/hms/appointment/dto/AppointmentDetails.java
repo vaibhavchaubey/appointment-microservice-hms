45ed0b3108c960c102d68da8a78485886315aac7
Vaibhav <vaibhavchaubey786@gmail.com>
2025-07-30 17:10:28 +0530

    Enhance AppointmentDetails and AppointmentServiceImpl to include patient and doctor email and phone fields

--- src/main/java/com/hms/appointment/dto/AppointmentDetails.java
+++ src/main/java/com/hms/appointment/dto/AppointmentDetails.java
@@ -13,8 +13,12 @@ public class AppointmentDetails {
     private Long id;
     private Long patientId;
     private String patientName;
+    private String patientEmail;
+    private String patientPhone;
     private Long doctorId;
     private String doctorName;
+    private String doctorEmail;
+    private String doctorPhone;
     private LocalDateTime appointmentTime;
     private Status status;
     private String reason;
